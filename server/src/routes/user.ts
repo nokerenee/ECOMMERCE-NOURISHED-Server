a84b1cca50ed0522948a04d7c1eef2cf56fb3c23
Nicole Kelly <nrkelly57@gmail.com>
2024-01-01 16:33:29 +1300

    added user login and password encryption

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import bcrypt from "bcrypt";
-import { UserModel } from "../models/user";
+import jwt from "jsonwebtoken";
+import { IUser, UserModel } from "../models/user";
 import { UserErrors } from "../errors";
 
 const router = Router();
@@ -20,6 +21,28 @@ router.post("/register", async (req: Request, res: Response) => {
     await newUser.save();
 
     res.json({ message: "User Registered Successfully" });
+  } catch (err) {
+    res.status(500).json({ type: err });
+  }
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const user: IUser = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ type: UserErrors.WRONG_CREDENTIALS });
+    }
+
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, userID: user._id });
   } catch (err) {
     res.status(500).json({ type: err });
   }
