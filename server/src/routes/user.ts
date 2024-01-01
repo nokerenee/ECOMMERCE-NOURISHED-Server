79fcc9052115b72d22184f104d515eb90a3fff1a
Nicole Kelly <nrkelly57@gmail.com>
2024-01-01 14:06:23 +1300

    setup user routes and error handling

---
+++ server/src/routes/user.ts
@@ -0,0 +1,28 @@
+import { Router, Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/user";
+import { UserErrors } from "../errors";
+
+const router = Router();
+
+router.post("/register", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.json({ message: "User Registered Successfully" });
+  } catch (err) {
+    res.status(500).json({ type: err });
+  }
+});
+
+export { router as userRouter };
