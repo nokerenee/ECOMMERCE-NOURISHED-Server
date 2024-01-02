e916e4d8366875c37bdb25505ace95485f414bb1
Nicole Kelly <nrkelly57@gmail.com>
2024-01-02 15:18:22 +1300

    added product model, routes and error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 
 import { userRouter } from "./routes/user";
+import { productRouter } from "./routes/product";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 mongoose.connect(
   "mongodb+srv://EcommerceProject123:NourishMe456@ecommerce.kcvbql0.mongodb.net/ecommerce"
---
+++ server/src/routes/product.ts
@@ -0,0 +1,68 @@
+import { Router, Request, Response } from "express";
+import { ProductModel } from "../models/product";
+import { UserModel } from "../models/user";
+import { verifyToken } from "./user";
+import { ProductErrors, UserErrors } from "../errors";
+
+const router = Router();
+
+router.get("/", verifyToken, async (_, res: Response) => {
+  try {
+    const products = await ProductModel.find({});
+    res.json({ products });
+  } catch (err) {
+    res.status(400).json({ err });
+  }
+});
+
+router.post("/checkout", verifyToken, async (req: Request, res: Response) => {
+  const { customerID, cartItems } = req.body;
+
+  try {
+    const user = await UserModel.findById(customerID);
+    const productIDs = Object.keys(cartItems);
+    const products = await ProductModel.find({ _id: { $in: productIDs } });
+
+    if (!user) {
+      return res.status(400).json({ type: UserErrors.NO_USER_FOUND });
+    }
+
+    if (products.length !== productIDs.length) {
+      return res.status(400).json({ type: ProductErrors.NO_PRODUCT_FOUND });
+    }
+
+    let totalPrice = 0;
+    for (const item in cartItems) {
+      const product = products.find((product) => String(product._id) === item);
+
+      if (!product) {
+        return res.status(400).json({ type: ProductErrors.NO_PRODUCT_FOUND });
+      }
+
+      if (product.stockQuantity < cartItems[item]) {
+        return res.status(400).json({ type: ProductErrors.NOT_ENOUGH_STOCK });
+      }
+
+      totalPrice += product.price * cartItems[item];
+    }
+
+    if (user.availableMoney < totalPrice) {
+      return res.status(400).json({ type: ProductErrors.NO_AVAILABLE_MONEY });
+    }
+
+    user.availableMoney -= totalPrice;
+    user.purchasedItems.push(...productIDs);
+    
+    await user.save();
+    await ProductModel.updateMany(
+      { _id: { $in: productIDs } },
+      { $inc: { stockQuantity: -1 } }
+    );
+    res.json({ purchasedItems: user.purchasedItems });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+export { router as productRouter };
