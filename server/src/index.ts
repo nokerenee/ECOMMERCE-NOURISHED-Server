8bf284558c1384b69b43bf36d36c4084935bbfd0
Nicole Kelly <nrkelly57@gmail.com>
2024-01-05 16:42:49 +1300

    optimize DB and server connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,8 +13,23 @@ app.use(cors());
 app.use("/user", userRouter);
 app.use("/product", productRouter);
 
-mongoose.connect(
-  "mongodb+srv://EcommerceProject123:NourishMe456@ecommerce.kcvbql0.mongodb.net/ecommerce"
-);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://EcommerceProject123:NourishMe456@ecommerce.kcvbql0.mongodb.net/ecommerce"
+    );
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB", err.message);
+  }
+};
 
-app.listen(3001, () => console.log("SERVER STARTED"));
+const startServer = async () => {
+  await connectDB();
+  const PORT = 3001;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
